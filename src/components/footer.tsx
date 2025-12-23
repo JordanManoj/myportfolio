8b519ce521a0c79ef2b5d67dde1cc43cefa45af7
Jordan Manoj <114975973+JordanManoj@users.noreply.github.com>
2025-12-23 23:33:49 +0530

    Remove footer attribution text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,11 +3,7 @@ import { Button } from '@/components/button';
 export const Footer = () => {
   return (
     <footer className="text-muted-foreground my-2 text-sm">
-      © {new Date().getFullYear()}{' '}
-      <Button variant="link" className="text-muted-foreground p-0 font-medium">
-        <a href="https://github.com/Skolaczk">Michał Skolak</a>
-      </Button>
-      . All rights reserved.
+      © {new Date().getFullYear()}. All rights reserved.
     </footer>
   );
 };
